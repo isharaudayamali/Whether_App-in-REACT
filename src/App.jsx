2d6a5afdff9ebfdfcb8d78ca5c492488cb9dd0ac
Ishara Udayamali <isharaudayamali220@gmail.com>
2025-09-26 22:53:07 +0530

    forcast create

--- src/App.jsx
+++ src/App.jsx
@@ -20,9 +20,11 @@ function App() {
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState("");
   const [error, setError] = useState("");
+  const [forcast, setForcast] = useState(null);
 
   useEffect(() => {}, []);
 
+  //fetch weather
   const fetchWeather = async (city) => {
     if (!city) return;
 
@@ -43,81 +45,151 @@ function App() {
     }
   };
 
-  //fetchWeather();
+  //fetch weather forcast
+  const fetchWeatherForcast = async (city) => {
+    if (!city) return;
+
+    try {
+      const response = await axios.get(
+        `https://api.openweathermap.org/data/2.5/forecast?q=galle&appid=52c3c1443d198e842e628cf6007c64a6`
+      );
+      setError("");
+      setForcast(response.data.list);
+      console.log(response.data);
+    } catch (error) {
+      console.log(error.response.data);
+      setWeather(null);
+      if (error?.response?.data?.message == "city not found") {
+        setError("City not found. Please try again.");
+      }
+    }
+  };
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
       console.log(e);
       fetchWeather(city);
+      fetchWeatherForcast(city);
     }
   };
 
   return (
-    <div className="whether">
-      {weather?.weather?.[0]?.icon && (
-        <img
-          src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
-          alt={`Weather icon for ${weather.weather[0].description}`}
-          className="weatherIcon"
-        />
-      )}
-      <h1>Weather App</h1>
-      <div className="search">
-        <input
-          type="text"
-          value={city}
-          placeholder="Enter city"
-          onChange={(e) => setCity(e.target.value)}
-          onKeyDown={handleKeyDown}
-        />
-        <img
-          src={searchIcon}
-          alt=""
-          onClick={() => {
-            fetchWeather(city);
-          }}
-        />
+    <div className="app">
+      <div className="whether">
+        {weather?.weather?.[0]?.icon && (
+          <img
+            src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+            alt={`Weather icon for ${weather.weather[0].description}`}
+            className="weatherIcon"
+          />
+        )}
+        <h1>Weather App</h1>
+        <div className="search">
+          <input
+            type="text"
+            value={city}
+            placeholder="Enter city"
+            onChange={(e) => setCity(e.target.value)}
+            onKeyDown={handleKeyDown}
+          />
+          <img
+            src={searchIcon}
+            alt=""
+            onClick={() => {
+              fetchWeather(city);
+              fetchWeatherForcast(city);
+            }}
+          />
+        </div>
+
+        <div>{error && <p className="error">{error}</p>}</div>
+
+        {weather && (
+          <div className="main-card">
+            <div>
+              <h2 className="location">
+                <img src={locationIcon} alt="" className="loca" />
+                {weather.name} - {weather.sys.country}
+              </h2>
+            </div>
+            <div>
+              <p>
+                <img src={cloudsIcon} alt="" className="cloudIcon" />
+                {weather.weather[0].main} - {weather.weather[0].description}
+              </p>
+            </div>
+            <div>
+              <p>
+                <img src={temperatureIcon} alt="" className="tempIcon" />
+                Temperature : {weather.main.temp}°C
+              </p>
+            </div>
+            <div>
+              <p>
+                <img src={humidityIcon} alt="" className="humidityIcon" />
+                Humidity : {weather.main.humidity}%
+              </p>
+            </div>
+            <div>
+              <p>
+                <img src={windIcon} alt="" className="windIcon" />
+                Wind Speed : {weather.wind.speed}m/s
+              </p>
+            </div>
+          </div>
+        )}
       </div>
 
-      <div>{error && <p className="error">{error}</p>}</div>
+      <div className="forcast">
+        {forcast &&
+          forcast.map((f, index) => (
+            <div className="forcast-card" key={index}>
+              <img
+                src={`https://openweathermap.org/img/wn/${f.weather[0].icon}@2x.png`}
+                alt={`Weather icon for ${weather.weather[0].description}`}
+                className=""
+              />
 
-  
+              <div>
+                {f.weather[0].main} - {f.weather[0].description}
+              </div>
 
-      {weather && (
-        <div className="main-card">
-          <div>
-            <h2 className="location">
-              <img src={locationIcon} alt="" className="loca" />
-              {weather.name} - {weather.sys.country}
-            </h2>
-          </div>
-          <div>
-            <p>
-              <img src={cloudsIcon} alt="" className="cloudIcon" />
-              {weather.weather[0].main} - {weather.weather[0].description}
-            </p>
-          </div>
-          <div>
-            <p>
-              <img src={temperatureIcon} alt="" className="tempIcon" />
-              Temperature : {weather.main.temp}°C
-            </p>
-          </div>
-          <div>
-            <p>
-              <img src={humidityIcon} alt="" className="humidityIcon" />
-              Humidity : {weather.main.humidity}%
-            </p>
-          </div>
-          <div>
-            <p>
-              <img src={windIcon} alt="" className="windIcon" />
-              Wind Speed : {weather.wind.speed}m/s
-            </p>
-          </div>
-        </div>
-      )}
+              <div>{f.dt_txt}</div>
+
+              <div>
+                <p>
+                  <img src={temperatureIcon} alt="" className="tempIcon" />
+                  Temperature : {f.main.temp}°C
+                </p>
+              </div>
+
+              <div>
+                <p>
+                  <img src={humidityIcon} alt="" className="humidityIcon" />
+                  Humidity : {weather.main.humidity}%
+                </p>
+              </div>
+
+              <div>
+                <p>
+                  <img src={windIcon} alt="" className="windIcon" />
+                  Wind Speed : {weather.wind.speed}m/s
+                </p>
+              </div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
 export default App;
+
+// import * as React from 'react';
+// import Card from '@mui/material/Card';
+// images/cards/contemplative-reptile.jpg"
+//raphy>
+//         </CardContent>
+//       </CardActionArea>
+//     </Card>
+//   );
+// }
